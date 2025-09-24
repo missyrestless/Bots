6ee3aecfa57045ef5a335fc676883e596c59a541
ron.record <ron.record@hornblower.com>
2025-09-24 12:33:43 -0700

    Add Nginx to service management scripts

--- slsvc.sh
+++ slsvc.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-all_services="corrade-anya corrade-easy shoutcast"
+# Adjust this list of services for your custom deployment
+all_services="corrade-anya corrade-easy nginx shoutcast"
 services=
 
 usage() {
-  printf "\nUsage: slsvc [-a] [-e] [-s] [-S service] [-u] [action]"
+  printf "\nUsage: slsvc [-a] [-e] [-n] [-s] [-S service] [-u] [action]"
   printf "\nWhere:"
   printf "\n\t-a indicates perform action on corrade-anya service"
   printf "\n\t-e indicates perform action on corrade-easy service"
+  printf "\n\t-n indicates perform action on nginx service"
   printf "\n\t-s indicates perform action on shoutcast service"
   printf "\n\t-S service specifies a service to perform action on"
-  printf "\n\t   if none of -a,-e,-s,-S is specified then services used are:"
+  printf "\n\t   if none of -a,-e,-n,-s,-S is specified then services used are:"
   printf "\n\t     ${all_services}"
   printf "\n\taction indicates the action to perform [default: status]"
   printf "\n\t    typical actions include start/stop/enable/disable/restart/status"
@@ -18,13 +20,16 @@ usage() {
   exit 1
 }
 
-while getopts ":aesS:u" flag; do
+while getopts ":aensS:u" flag; do
   case $flag in
     a)
       services="${services} corrade-anya"
       ;;
     e)
       services="${services} corrade-easy"
+      ;;
+    n)
+      services="${services} nginx"
       ;;
     s)
       services="${services} shoutcast"
@@ -48,8 +53,15 @@ action="$1"
 
 [ "${services}" ] || services="${all_services}"
 
+if [[ $EUID -eq 0 ]]
+then
+  SUDO=
+else
+  SUDO=sudo
+fi
+
 export TERM="xterm-256color"
 for script in ${services}
 do
-  sudo systemctl ${action} ${script}.service
+  ${SUDO} systemctl ${action} ${script}.service
 done
